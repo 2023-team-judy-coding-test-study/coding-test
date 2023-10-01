5468c005c0bcd3aa45fcd7fca0eb8cd56be1f896
Gnu-Kenny <qkr0677@gmail.com>
2023-10-02 01:34:00 +0900

    add 'n과m 4'

--- geunwoo/3주차/BJ_N과M4/main.py
+++ geunwoo/3주차/BJ_N과M4/main.py
@@ -1,42 +1,34 @@
-import sys
-input = sys.stdin.readline
-
-n = 0
-m = 0
-sqnc = []
-visit = []
+n: int
+m: int
+visited = []
 answer = []
-answers = []
 
 
-def back(level: int):
-    global n, m, sqnc, answer, visit, answers
+def backtracking(idx: int, level: int):
+    global n, m, answer, visited
+    # 종료 조건
     if level == m:
-        if sorted(answer) not in answers:
+        if sorted(answer) in visited:
+            print(answer)
+            print(visited)
+            return
 
-            print(*answer)
-            answers.append(sorted(answer))
-
+        print(*answer)
+        visited.append(sorted(answer))
         return
 
-    for i in range(len(sqnc)):
-        if visit[sqnc[i]]:
-            continue
-        visit[sqnc[i]] = 1
-        answer.append(sqnc[i])
-        back(level+1)
-        visit[sqnc[i]] = 0
+    # 가지 치기
+    for i in range(idx, n+1):
+        answer.append(i)
+        backtracking(i, level+1)
         answer.pop()
 
 
 def main():
-    global n, m, sqnc, answer, visit
-    n, m = list(map(int, input().split()))
-    visit = [0 for _ in range((10000 + 1))]
-    sqnc = list(map(int, input().split()))
-    sqnc.sort()
+    global n, m
+    n, m = map(int, input().split())
 
-    back(0)
+    backtracking(1, 0)
 
 
 if __name__ == "__main__":
