1582b943ba77e7beb96804838207086ebf5fb3a8
Ellie Kim <yulim.dev@gmail.com>
2023-10-14 03:31:07 +0900

    sol: n진수 게임

---
+++ yulim/4주차/PR_n진수게임.py
@@ -0,0 +1,21 @@
+def solution(n, t, m, p):
+    answer = ""
+    data = "-0"  # 0일때
+    nums = "0123456789ABCDEF"
+
+    for i in range(t * m):
+        tmp = ""
+        s = i
+
+        while s > 0:  # n진수로 변환
+            s, mod = divmod(s, n)
+            tmp += nums[mod]
+        data += tmp[::-1]
+
+    for i in range(p, t * m + 1, m):
+        answer += data[i]
+
+    return answer
+
+
+print(solution(2, 4, 2, 1))
