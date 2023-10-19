daf59182636b0aaf5407efb79a70b521dc234cf7
Ellie Kim <yulim.dev@gmail.com>
2023-10-19 18:41:16 +0900

    sol: 캐시

--- yulim/5주차/PR_캐시.py
+++ yulim/5주차/PR_캐시.py
@@ -1,13 +1,13 @@
 def solution(cacheSize, cities):
     answer = 0  # 실행 시간
 
-    cities = [c.lower() for c in cities]
     cache = []
 
     if cacheSize == 0:
         return 5 * len(cities)
 
     for city in cities:
+        city = city.lower()
         if city in cache:
             answer += 1
             cache.append(cache.pop(cache.index(city)))
