3f89aad40509e653abe2f9cd5f30bc7f8cbf20d0
Gnu-Kenny <qkr0677@gmail.com>
2023-10-22 20:48:11 +0900

    코드 오류 수정

--- geunwoo/아카이브/BJ_19236/main.py
+++ geunwoo/아카이브/BJ_19236/main.py
@@ -1,8 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-from heapq import heapify, heappop, heappush
-from collections import deque
 from typing import *
 from copy import deepcopy
 
@@ -22,43 +20,40 @@ class Node:
 dy = [-1, -1, 0, 1, 1, 1, 0, -1]
 dx = [0, -1, -1, -1, 0, 1, 1, 1]
 
-g_board = []
-g_dir_board = []
-
 result = 0
 
 
 def main():
-    global g_board, g_dir_board
     """
     7 6 2 3 15 6 9 8
     3 1 1 8 14 7 10 1
     6 1 13 6 4 3 11 4
     16 1 8 7 5 2 12 2
     """
+    board = []
+    dir_board = []
     for _ in range(4):
         row = list(map(int, input().split()))
         
-        g_board.append(row[::2])
-        g_dir_board.append(list(map(lambda x: x-1,row[1::2])))
+        board.append(row[::2])
+        dir_board.append(list(map(lambda x: x-1,row[1::2])))
         
     
-    dfs(g_board, g_dir_board, Node(0, 0, 0), 0)    
+    dfs(board, dir_board, Node(0, 0, -1), 0)    
     print(result)
     
 
 def move_all_fishes(board: List[list], dir_board: List[list], shark: Node):
-    
     for i in range(1, 16 + 1):
         now: Optional[Node] = find_fish(board, i)
         
         if now is None:
-            return
+            continue
         
         direction = dir_board[now.y][now.x]
         for j in range(8):
-            ny = now.y + dy[j]
-            nx = now.x + dx[j]
+            ny = now.y + dy[direction]
+            nx = now.x + dx[direction]
             
             # 이동할 수 없을때 - 좌표를 벗어남
             if ny < 0 or nx < 0 or ny >= 4 or nx >= 4:
@@ -72,10 +67,10 @@ def move_all_fishes(board: List[list], dir_board: List[list], shark: Node):
             
             dir_board[now.y][now.x] = direction
             
-            board[shark.y][shark.x], board[ny][nx] = board[ny][nx], board[shark.y][shark.x]
-            dir_board[shark.y][shark.x], dir_board[ny][nx] = dir_board[ny][nx], dir_board[shark.y][shark.x]
+            board[now.y][now.x], board[ny][nx] = board[ny][nx], board[now.y][now.x]
+            dir_board[now.y][now.x], dir_board[ny][nx] = dir_board[ny][nx], dir_board[now.y][now.x]
             break
-
+    
 
 def find_fish(board: List[list], idx: int):
     for i in range(4):
@@ -102,9 +97,11 @@ def get_possible_pos(board: List[list], dir_board: List[list], shark: Node):
             continue
         
         if board[ny][nx] == -1:
+            shark = Node(ny, nx, shark.w)
             continue
         
         positions.append(Node(ny, nx, board[ny][nx]))
+        shark = Node(ny, nx, shark.w)
     
     return positions
 
@@ -119,7 +116,7 @@ def dfs(board: List[list], dir_board: List[list], shark: Node, total):
     board[shark.y][shark.x] = -1
     
     move_all_fishes(board, dir_board, shark)
-    
+
     positions = get_possible_pos(board, dir_board, shark)
     
     if len(positions) == 0:
@@ -127,7 +124,8 @@ def dfs(board: List[list], dir_board: List[list], shark: Node, total):
         return
     
     for pos in positions:
-        dfs(board, dir_board, Node(pos.y, pos.x, pos.w), total)
+        shark = Node(pos.y, pos.x, -1)
+        dfs(board, dir_board, shark, total)
     
 if __name__ == '__main__':
     main()
