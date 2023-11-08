3c6168abdd32ad62e3fc33264b9adda63486d33b
Ellie Kim <yulim.dev@gmail.com>
2023-11-08 21:25:35 +0900

    sol: PR_택배배달과수하기

--- yulim/6주차/PR_택배배달과수거하기.py
+++ yulim/6주차/PR_택배배달과수거하기.py
@@ -1,25 +1,19 @@
-# 그리디
-# 먼 곳 부터 탐색
-
 def solution(cap, n, deliveries, pickups):
-    answer = 0
-    
     deliveries = deliveries[::-1]
     pickups = pickups[::-1]
-
-    deliver = 0
-    pickup = 0
-
-    for i in range(n):
-
-        deliver += deliveries[i]
-        pickup += pickups[i]
-
-        while deliver > 0 or pickup > 0:
-            deliver -= cap
-            pickup -= cap
-            answer += (n - i) * 2
+    answer = 0
     
+    d = 0
+    p = 0
+    
+    for i in range(n):
+        d += deliveries[i]
+        p += pickups[i]
+        
+        while d > 0 or p > 0:
+            d -= cap
+            p -= cap
+            answer += (n - i) * 2
     return answer
 
 
