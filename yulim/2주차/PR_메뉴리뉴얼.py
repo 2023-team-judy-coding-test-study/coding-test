891ceea1fbb86cfbe39fb9dffef6abaf194f9a19
Ellie Kim <yulim.dev@gmail.com>
2023-12-13 18:47:04 +0900

    sol: PR_메뉴리뉴얼

---
+++ yulim/2주차/PR_메뉴리뉴얼.py
@@ -0,0 +1,39 @@
+def dfs(menu, tmp, order, length, depth, cur): 
+    if depth == length:
+        tmp = ''.join(sorted(tmp))
+        menu[tmp] = menu.get(tmp, 0) + 1
+        return
+    
+    for i in range(cur, len(order)):
+        tmp[depth] = order[i]
+        dfs(menu, tmp, order, length, depth+1, i+1)
+    
+
+def solution(orders, course):
+    answer = []
+    menu = {}
+
+    for order in orders:
+        for length in course:
+            tmp = [0] * length
+            dfs(menu, tmp, order, length, 0, 0)
+
+    result = [0]*(max(course)+1)
+
+    for k, v in menu.items():
+        if len(k) in course:
+            if v >= 2 and v >= result[len(k)]:
+                result[len(k)] = v
+    
+
+    for length in course:
+        for k, v in menu.items():
+            if len(k) == length and v == result[length]:
+                answer.append(k)
+
+    return sorted(answer)
+
+
+print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4]))
+print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"],[2,3,5]))
+print(solution(["XYZ", "XWY", "WXA"],[2,3,4]))
